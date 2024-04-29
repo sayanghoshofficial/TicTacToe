d7ca08ac966b6bd8e0acb3cb8542789ab606294e
Sayan Ghosh <sayanghoshofficial@users.noreply.github.com>
2024-04-29 19:40:27 +0530

    add MsgComp

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { BoardWpr, BoardComp, Box } from './Board.styled'
+import { BoardWpr, BoardComp, Box, MsgComp,Text,Button} from './Board.styled'
 
 const initialBoard = Array(9).fill(null);
 const Board = () => {
     return (
         <BoardWpr>
+        <MsgComp>
+        <Text>
+        X Player Turn
+        </Text>
+        <Button>Reset</Button>
+        </MsgComp>
             <BoardComp>
                 {initialBoard.map((elem, idx) => (
                     <Box />
--- src/components/Board/Board.styled.js
+++ src/components/Board/Board.styled.js
@@ -4,18 +4,37 @@ export const BoardWpr = styled.div`
     width:100vw;
     height: 100vh;
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
-    border: 2px solid;
 `
 
 export const BoardComp = styled.div`
     display: grid;
     grid-template-columns: auto auto auto;
-    border: 2px solid red;
+    gap:10px;
+    /* border: 2px solid red; */
 `
 export const Box = styled.div`
     width:100px;
     height:100px;
     border:2px solid green;
     `
+
+export const MsgComp = styled.div`
+    width:330px;
+    text-align:center;
+    padding-bottom: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    /* border:2px solid green; */
+    font-size: 30px;
+    `
+export const Text = styled.p`
+    font-size: 30px;
+
+    `;
+export const Button = styled.button`
+    padding:  10px;
+    `;
